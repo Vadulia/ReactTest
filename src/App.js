dbd448398e0bb0d0083a240c6abcff4e41106ca9
Vadulia <honda2907@gmail.com>
2022-04-12 10:16:19 +0300

    test1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TourCards from "./componerts/TourCards";
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
-function App() {
+
+
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <Container sx={{marginY:15}}>
+         <Grid container spacing={4}>
+      <TourCards />
+      <TourCards />
+      <TourCards />
+      <TourCards />
+      </Grid>
+      </Container>
     </div>
   );
 }
